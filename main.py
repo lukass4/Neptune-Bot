b2609b6377e39fd905c9fec0b2e910d4a548befa
Lukas <76885856+LJ-exe@users.noreply.github.com>
2021-02-24 15:14:08 +0000

    A

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ async def on_ready():
   print("Bot is online! {0.user}".format(client))
 
 
+
 @client.event
 async def on_message(message):
-  if message.author == client.user:
-    return
+
 
   if message.content.startswith(prefix + "hello"):
     await message.channel.send("You just said hello to a bot. Get some friends.")
@@ -36,8 +36,11 @@ async def on_message(message):
     await message.channel.send("__**The server has been deleted!**__")
     print("Someone tried to delete the server.")
 
-  
   if message.content.startswith(prefix + "help"):
     await message.channel.send("Hi I am neptune bot. My prefix is `" + prefix + "`. Try `" + prefix + "commands` to get started!")
+
+  if message.content.startswith(prefix + "spam"):
+    await message.channel.send(prefix + "spam")
+
 
 client.run("ODE0MTAyMDYxOTIyMTIzODM2.YDY9oA.rcel_4RUzVB5Kxa076ZYJmDPvDw")
