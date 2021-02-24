9df5d9d8cad2c2b4cd7ab3f889e3f11ebad4ebc0
Lukas <76885856+LJ-exe@users.noreply.github.com>
2021-02-24 13:49:30 +0000

    1.0.1
    Added more commands and fixed the token

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import os
 import discord
+import time
 
 client = discord.Client()
 
-default_prefix = ("$")
+default_prefix = ("~")
 prefix = (default_prefix)
 
 @client.event
 async def on_ready():
-  print("Bot is online! ({0.user}".format(client))
+  print("Bot is online! {0.user}".format(client))
 
 
 @client.event
@@ -19,4 +20,13 @@ async def on_message(message):
   if message.content.startswith(prefix + "hello"):
     await message.channel.send("You just said hello to a bot. Get some friends.")
 
-client.run(os.getenv("TOKEN"))
+  if message.content.startswith(prefix + "nuke"):
+    await message.channel.send("**BOOM!**")
+    time.sleep(2)
+    await message.channel.send("__**The server has been deleted!**__")
+    print("Someone tried to delete the server.")
+
+  if message.content.startswith("@{0.user}".format(client)):
+    await message.channel.send("Hi I am neptune bot. My prefix is `" + prefix + "`. Try `" + prefix + "help` to get started!")
+
+client.run("ODE0MTAyMDYxOTIyMTIzODM2.YDY9oA.rcel_4RUzVB5Kxa076ZYJmDPvDw")
